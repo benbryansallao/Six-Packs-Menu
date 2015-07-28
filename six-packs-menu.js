73efc5b422c6a3ce84046ac370e5079dc0baa82a
Ben Bryan Sallao <benbryansallao@users.noreply.github.com>
2015-07-28 09:49:27 +0800

    Update six-packs-menu.js

--- six-packs-menu.js
+++ six-packs-menu.js
@@ -1,8 +1,1 @@
-$(document).ready(function Firstchild() {
-  $(".first-child-menu").click(function () {
-	$(".play, .first-child-transition-overlay").animate({
-		"width":"100%",
-		"display":"block"
-	}, 3000); 
-});
-});
+
